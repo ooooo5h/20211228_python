0531dea0afb2f0bad6048984250b671155fa6763
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 10:31:50 +0900

    append 모드 체험
    - a : append

--- main01.py
+++ main01.py
@@ -1,5 +1,5 @@
-my_file = open('내파일.txt', 'w')   # w : write 모드로 파일 열기
+my_file = open('내파일.txt', 'a')   # w : write 모드로 파일 열기
 
-my_file.write('고속터미널 볼 거 많았는데!! 또 가야지\n')
-my_file.write('신 젤리 먹구싶다.\n춥지만 따뜻해서 하품이 난다요 \n')
+my_file.write('원주 까치둥지 짱 맛있지\n')
+
 my_file.close()
