22f2d9d3fb8622569f437bb7de5da23d507126a0
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 11:19:06 +0900

    readlines 기능 활용
    - 모든 내용을 미리 list에 담아두고
    - for문 이용해서 내용을 출력

--- main03.py
+++ main03.py
@@ -2,17 +2,14 @@
 
 my_file = open('c:\\202112_EunHyung_Python\\file_test\\my_test3.txt', 'r')  # r : read 읽어오기 모드
 
-# 파일 내용 전부 읽고 싶다 => 몇 줄이 전부일까? 알 수 없음
-# 반복 횟수 불명확함 => while True + if break 용법 활용
-while True:
-    line = my_file.readline()
-    
-    # 내용 출력 : 읽어온 내용이 있어야 의미있는 행위
-    # 읽어온 내용에 아무것도 없다 : 반복 종료
-    if not line:
-        print('파일을 끝까지 읽었습니다.')
-        break
-     
-    print(line, end = '')
+# my_file의 모든 내용을 미리 파악해둔다면? 필요한 만큼 반복 돌리면 된다
+# 미리 파악 => readlines() => 모든 줄을 미리 읽어서, list로 담아준다
+
+line_list = my_file.readlines()
+
+# 리스트 내부의 요소들을 반복으로 뽑아내서 출력해보자
+# for문의 정석 방법
+for line in line_list:
+    print(line)
     
 my_file.close()
