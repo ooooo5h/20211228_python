2ffb59d4a36806fcee33f5aa36585ba444cfcb79
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 12:04:33 +0900

    한 줄 str -> split해서 각각을 가공해 하나의 문장으로 재가공

--- main05.py
+++ main05.py
@@ -10,5 +10,15 @@ with open('student_list.txt', 'r') as f:
         
         # student_info str 끝에는 \n이 달려있다 => 임시 제거 처리
         student_info = student_info.strip()
+          
+        # 조경진(34세) - 서울시 동대문구 거주   양식으로 가공해서 출력
+        # student_info를 ',' 기준으로 쪼개줘야 이름/출생년도/거주지를 분리해서 얻을 수 있다 => 구글링
+        student_infos = student_info.split(',')   # ,를 기준으로 나눠서, 나온 목록을 변수에 저장
         
-        print(student_info)
+        # 이름 / 나이(int , 출생년도 활용) / 거주지
+        name = student_infos[0]
+        age = 2021 - int(student_infos[1]) + 1
+        address = student_infos[2]
+        
+        info_str = f'{name}({age}세) - {address}'
+        print(info_str)
