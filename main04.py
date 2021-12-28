75c53cba1e5293c2890beffaf38bb5fc5ad3a6e5
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 11:43:20 +0900

    학생 정보 직접 입력 기능
    - 한줄로 가공해서, 파일에 넣을 준비

--- main04.py
+++ main04.py
@@ -2,7 +2,18 @@
 # 파이썬의 with 문법 이용하면, 열고 난 파일을 with 구문이 끝나면 자동으로 종료처리
 
 with open('student_list.txt', 'a') as my_file:    # a : 추가 작성이지만, 해당 파일이 없다면 생성도 같이 진행해줌
-    my_file.write('이 파일은 학생 목록을 저장합니다.')
+
+    # 새 학생을 한명씩 추가하자
+    # 이름, 출생년도, 거주지 (ex. 서울시 동대문구)
+    print('새 학생 추가')
+    
+    name = input('이름 : ')
+    birth_year = int(input('출생년도 : '))
+    address = input('거주지 : ')
+    
+    # 파일에 한 줄의 문구로 추가 = 이름, 년도, 거주지 한 줄로 가공해서 파일에 추가하고싶다
+    input_line = f'{name}, {birth_year}, {address}'
+    print(input_line)
     
 # with 구문이 끝남
 # 여기가 실행된다 : my_file은 자동으로 close되어있는 상태
