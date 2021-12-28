0111bbcb571dc887aba538b6d0cd97cde441af47
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 12:15:15 +0900

    학생 클래스에 메쏘드 2개 추가
    - 나이 계산해서 int로 리턴
    - 가공된 문구로 정보 출력

--- datas/student.py
+++ datas/student.py
@@ -6,3 +6,13 @@ class Student:
         self.name = name
         self.birth_year = int(birth_year)    # str이 들어온 경우를 대비한다
         self.address = address
+
+    
+    # 나이를 계산해주는 기능 추가(2021년 기준)
+    def get_age(self):     # 파라미터값에 birth_year은 안들어감 왜냐 이미 self에 담겨있기 때문.. 세상에 깨달음!
+        return 2021 - self.birth_year + 1
+    
+    
+    # 정보를 가공해서 출력(print)기능 추가 = return X
+    def print_student_info(self):
+        print(f'{self.name}({self.get_age()}세) - {self.address}')
