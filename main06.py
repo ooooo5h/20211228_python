bf22dc12f5d855083b00618a31d1c9890fcf10d1
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 12:20:36 +0900

    str -> 분해해서 -> 학생 객체로 만들어서 기능 활용

--- main06.py
+++ main06.py
@@ -1,6 +1,11 @@
+from datas.student import Student
+
 # 파일의 내용중, 첫 학생이 누구인지만 출력
 with open('student_list.txt', 'r') as f:
     line = f.readline().strip()   
     
-    # 문제 : 조경진(34세) - 서울시 동대문구 다시 가공해서 출력
-    print(line)
+    # 문제 : 조경진(34세) - 서울시 동대문구 다시 가공해서 출력 => 학생 객체를 만들고, 기능 활용
+    infos = line.split(',')
+    
+    student = Student( infos[0], infos[1], infos[2] )   # 학생 객체로 생성
+    student.print_student_info()
