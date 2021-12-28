5ec8a22b53b8c200c7f7112ac90fd815e2ab9da4
ooooo5h <ooooo5h91@gmail.com>
2021-12-28 11:32:44 +0900

    모든 내용 한번에 변수에 담아주기

--- main03.py
+++ main03.py
@@ -2,17 +2,8 @@
 
 my_file = open('c:\\202112_EunHyung_Python\\file_test\\my_test3.txt', 'r')  # r : read 읽어오기 모드
 
-# my_file의 모든 내용을 미리 파악해둔다면? 필요한 만큼 반복 돌리면 된다
-# 미리 파악 => readlines() => 모든 줄을 미리 읽어서, list로 담아준다
-
-line_list = my_file.readlines()
-
-# 리스트 내부의 요소들을 반복으로 뽑아내서 출력해보자
-# for문의 정석 방법
-for line in line_list:
-    
-    # 읽어온 한 줄의 \n은 제거하자
-    line = line.strip()   # 마지막의 \n 제거한 새로운 str을 리턴하는 기능
-    print(line)
+# 파일의 전체 내용이 통째로 필요한 경우
+full_data = my_file.read()
+print(full_data)
     
 my_file.close()
